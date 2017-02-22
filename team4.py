028bd0f691c17650c1b643a445a356be3a38950e
EzequielPimentel <023932@daltonK12.onmicrosoft.com>
2017-02-22 09:27:51 -0500

    Update team4.py
    'ArtPony'
    changed the idea to grim(all betray) strategy 
    edited the team4 ('ArtPony') code line

--- team4.py
+++ team4.py
@@ -9,17 +9,29 @@
 team_name = 'ArtPony'
 strategy_name = 'Betray'
 strategy_description = 'Always betray.'
-    
-def move(my_history, their_history, my_score, their_score):
+  def move(my_history, their_history, my_score, their_score):
     '''Make my move based on the history with this player.
+
     
+
     history: a string with one letter (c or b) per round that has been played with this opponent.
+
     their_history: a string of the same length as history, possibly empty. 
+
     The first round between these two players is my_history[0] and their_history[0]
+
     The most recent round is my_history[-1] and their_history[-1]
+
     
+
     Returns 'c' or 'b' for collude or betray.
+
     '''
-    
-    #This example player always betrays.      
-    return 'b'
+
+    if 'b' in their_history:
+
+        return 'b'
+
+    else:
+
+        return 'b'
